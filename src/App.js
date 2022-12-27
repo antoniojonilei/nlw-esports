8cc140ae9f723c9f56ea6e20d6f78f6caf11c8fc
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-12-27 17:46:14 -0300

    first secction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import ListItem from "./components/ListItem";
 import Section from "./components/Section";
 
 import dbfz from "./assets/img/dragon-ball-fighterz.jpg"
+import ssbu from "./assets/img/smash-bros-ultimate.jpg"
+import grid from "./assets/img/grid-autosport.jpg"
+import sfv from "./assets/img/street-fighter.jpg"
+
+const gamesListData = [
+  {
+    url: "https://www.twitch.tv/directory/game/Dragon%20Ball%20FighterZ",
+    imageUrl: dbfz,
+    alt: "Imagem do jogo Dragon Ball FighterZ"
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Super%20Smash%20Bros.%20Ultimate",
+    imageUrl: ssbu,
+    alt: "Imagem do jogo Super Smash Bros Ultimate"
+  },
+  {    
+    url: "https://www.twitch.tv/directory/game/GRID%3A%20Autosport",
+    imageUrl: grid,
+    alt: "Imagem do jogo Grid AutoSport"
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Street%20Fighter%20V",
+    imageUrl: sfv,
+    alt: "Imagem do jogo Street Fighter V" 
+  } 
+]
 
 function App() {
   return (
@@ -11,7 +37,13 @@ function App() {
       <main>
         <Section title="Meus Jogos" subtitle="Os games que eu mais curto jogar!">
 
-          <ListItem url="https://www.twitch.tv/directory/game/League%20of%20Legends" imageUrl={dbfz} alt="Imagem do jogo League of Legends"/>
+          {gamesListData.map((item) => (
+            <ListItem 
+              url={item.url} 
+              imageUrl={item.imageUrl} 
+              alt={item.alt}
+            />
+          ))}       
 
         </Section>  
         
